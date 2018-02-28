38ceb9ca762e6f071d19548233c196ec9a75f621
Rob Hom <RLH5830@gmail.com>
2018-02-27 23:56:34 -0500

    styled county on support and discounts. Moved javascript to script.js. Added select2 styling to css

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -6,15 +6,28 @@ inputs.forEach(input => {
     event => {
       input.classList.add("error");
       document.querySelector('#please-complete').style.display = 'block';
-      
+
     },
     false
   );
 });
 
-	
+
 $(function(){
  	$(".card").flip ({
         trigger: 'hover'
    		});
-			});	
+			});
+
+
+$(document).ready(function() {
+    $('.smultiple').select2();
+});
+$('.js-example-basic-single').select2();
+// $(document).ready(function() {
+//     $('.ssingle').select2();
+// });
+// $("#county").select2({
+// placeholder: 'Select One',
+// allowClear: true
+// });
