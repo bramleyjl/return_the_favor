b2b49826d6877e4ffef7e0f97f98113151235d7d
Rob Hom <RLH5830@gmail.com>
2018-03-12 13:21:00 -0400

    Can't find the error

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,30 +1,30 @@
 const inputs = document.querySelectorAll("input, select, textarea");
 
 inputs.forEach(input => {
-  input.addEventListener(
-    "invalid",
-    event => {
-      input.classList.add("error");
-      document.querySelector('#please-complete').style.display = 'block';
+    input.addEventListener(
+        "invalid",
+        event => {
+            input.classList.add("error");
+            document.querySelector('#please-complete').style.display = 'block';
 
-    },
-    false
-  );
+        },
+        false
+    );
 });
 
 $(document).ready(function() {
-  $('.smultiple').select2();
-  var configParamsObj = {
-      placeholder: 'Choose One',
-      minimumResultsForSearch: 1,
-      dropdownAutoWidth: true
-  };
-  $(".single").select2(configParamsObj);
-  });
+    $(".smultiple").select2();
+    var configParamsObj = {
+        placeholder: 'Choose One',
+        minimumResultsForSearch: 1,
+        dropdownAutoWidth: true
+    };
+    $(".single").select2(configParamsObj);
+});
 
 
-$(document).ready(function(){
- 	$(".card").flip ({
+$(document).ready(function() {
+    $(".card").flip({
         trigger: 'hover'
-   		});
-			});
+    });
+});
