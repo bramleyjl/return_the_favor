8fb661c290e6102fc88697f387329128bc900cf4
John <bramleyjl@gmail.com>
2018-03-12 20:16:19 -0400

    process.env database login credentials added

--- db.js
+++ db.js
@@ -2,12 +2,20 @@
 var mysql = require('mysql');
 var settings = require('./config/config.json')
 
+console.log('process.env not in use', process.env.DB_HOST)
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
+console.log('process.env in use!', process.env.DB_HOST)
+
 var connection = mysql.createConnection({
-    host     : settings.db.host,
-    port     : settings.db.port,
-    user     : settings.db.user,
-    password : settings.db.password,
-    database : settings.db.database
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_PORT,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DATABASE
 });
 
 connection.connect(function(err) {
